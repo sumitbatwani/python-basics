ab6ad84bc6e125b9f590fad2b270204135e3859d
Sumit kumar <sumit.k@tyroo.com>
2020-08-07 01:06:20 +0530

    initial commit

---
+++ basics.py
@@ -0,0 +1,57 @@
+# Print on console
+print("Python is case sensitive language!")
+
+# - Input -
+# name = input('What is your name? ')
+# color = input('What is your favorite colour? ')
+# print(name + ' likes ' + color)
+
+# - Type Convertion (int(), float(), bool()) -
+# birth_year = input("Input your birth year to get your age? ")
+# age = 2020 - int(birth_year)
+# print("Your age based on birth year is", age)
+
+# - Strings -
+# name = "Sumit"
+
+# - Multi-line Strings -
+# email_description = """
+# Hi Sumit,
+
+# It was nice to work with you!
+
+# Regards,
+# Someone. """
+
+# print(email_description)
+
+# - Negative index char -
+# It starts to print from the end.
+
+# name = "Sumit Batwani"
+# print(name[-1])
+
+# - Formatted Strings -
+# first = "Sumit"
+# last = "Batwani"
+
+# msg = f"{first} [{last}]"
+# print(msg)
+
+# - String method find returns indexOf char and in returns boolean -
+# msg = "Python for Beginners"
+# print(msg.find("P"))
+# print("Python" in msg)
+
+# - Arithematic Operator -
+# print("float ", 10 / 3)
+# print("int ", 10 // 3)
+# print("to the power ", 10 ** 3)
+
+# - Operator Precedence -
+# paranthesis
+# exponential 2 ** 2 (4)
+# mulitplication or division
+# addition or subtraction (PE-BODMAS)
+# x = 10 + 3 * 2 ** 2
+# print(x) equals to (22)
---
+++ conditions.py
@@ -0,0 +1,18 @@
+# - if-elif-else -
+# is_hot = True
+# is_cold = False
+
+# if is_hot:
+#     print("It is a hot day")
+# elif is_cold:
+#     print("It is a cold day")
+# else:
+#     print("It is a good day")
+
+# Another example
+# price = 1000000
+# has_good_credit = False
+# if has_good_credit:
+#     print(f"Down Payment ${price * (10 / 100)}")
+# else:
+#     print(f"Down Payment ${price * (20 / 100)}")
